0a99fb99825c8a8c997226ccf92110c393a4e7a8
vereczkibalint <vereczkibalint@gmail.com>
2019-11-19 15:37:07 +0100

    Added reading from file function

--- script.sh
+++ script.sh
@@ -32,18 +32,23 @@ echo "Emailek beolvasása"
 emailCounter=0
 while IFS=';' read -r cimzett targy uzenet
 do
-    emailCounter=$((emailCounter+1))
-    echo "$emailCounter. üzenet küldése"
-    RESPONSE=`curl -fSs --user 'api:980ac2043c279591ba48555f9f0affda-09001d55-ec4b639e' \
-        https://api.mailgun.net/v3/sandboxee0fa03278b6491c93e4dd2b6d1c73d2.mailgun.org/messages \
-        -F from="$KITOL" \
-        -F to="$cimzett" \
-        -F subject="$targy" \
-        -F text="$uzenet"`
+    if [[ "$cimzett" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]
+    then
+      emailCounter=$((emailCounter+1))
+      echo "$emailCounter. üzenet küldése"
+      RESPONSE=`curl -fSs --user 'api:key-fdf52eb232c49f2ede4e9e508eb52a37' \
+          https://api.mailgun.net/v3/sandboxee0fa03278b6491c93e4dd2b6d1c73d2.mailgun.org/messages \
+          -F from="$KITOL" \
+          -F to="$cimzett" \
+          -F subject="$targy" \
+          -F text="$uzenet"`
 
-    if [ $? -gt 0 ]; then
-    echo "Hiba történt: $RESPONSE"
-    exit 1
+      if [ $? -gt 0 ]; then
+      echo "Hiba történt: $RESPONSE"
+      exit 1
+      fi
+    else
+      echo "Hibás email cím formátum!"
     fi
 done <emailek.log
 echo "Emailek kiküldve"
